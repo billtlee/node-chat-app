23de0805b719040a8b6b777289533b1537f6405c
billtlee <wlee3@hotmail.com>
2018-12-10 18:28:59 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'jenny',
-    text: 'Hello.'
-  });
 });
 
 socket.on('disconnect', () => {
